96fd1d1ff85e195de62e99caa1d9cbfeb944679f
lumabanls <intern1@lifesaver.care>
2025-08-02 09:02:29 +0800

    chore: merge from main into develop

--- tshirt-store-backend/obj/Debug/net9.0/tshirt-store-backend.AssemblyInfo.cs
+++ tshirt-store-backend/obj/Debug/net9.0/tshirt-store-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tshirt-store-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923e215cb2bd2f6bdee7a693ba4dd82ec6fcc8c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b91e32e904c8affcf748112444cdd30196263557")]
 [assembly: System.Reflection.AssemblyProductAttribute("tshirt-store-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tshirt-store-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
